4985774541155c9d220b2a545ebec08de852ec4c
Nguyen Quang Hoc <hoc.nguyen@meganet.com.vn>
2018-06-13 20:08:28 +0700

    Save

---
+++ homepage/js/custum.js
@@ -0,0 +1,19 @@
+﻿$.fn.responsiveTabs = function() {
+  this.addClass('responsive-tabs');
+  this.append($('<span class="fas fa-bars"></span>'));
+  this.append($('<span class="fas fa-times"></span>'));
+
+  this.on('click', 'li.active > a, span.fas', function() {
+    this.toggleClass('open');
+  }.bind(this));
+
+  this.on('click', 'li:not(.active) > a', function() {
+    this.removeClass('open');
+  }.bind(this));
+};
+
+$('.nav.nav-tabs').responsiveTabs();
+$(function() {
+    $('.height').matchHeight();
+   
+});
